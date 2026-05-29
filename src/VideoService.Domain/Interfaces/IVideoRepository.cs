3f5e96ad4f7b5635a347f44231a4a632faf41bdd
Alnils <Albin.nilsson.se@gmail.com>
2026-05-29 18:59:27 +0200

    created interface

---
+++ src/VideoService.Domain/Interfaces/IVideoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoService.Domain.Entities;
+
+
+namespace VideoService.Domain.Interfaces;
+
+public interface IVideoRepository
+{
+    Task<Video?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<Video?> GetByCourseAndLessonAsync(int courseId, int lessonId, CancellationToken ct = default);
+    Task<List<Video>> GetByCourseIdAsync(int courseId, CancellationToken ct = default);
+    Task<Video> AddAsync(Video video, CancellationToken ct = default);
+    Task<bool> DeleteAsync(int id, CancellationToken ct = default);
+}
