efbf44dfd9dd11f5003dd82e3d515bbfdf875199
Alnils <Albin.nilsson.se@gmail.com>
2026-05-29 18:33:30 +0200

    created video entity

---
+++ src/VideoService.Domain/Entities/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoService.Domain.Entities;
+
+public class Video
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string BlobPath { get; set; } = string.Empty;
+    public string ContentType { get; set; } = "video/mp4";
+    public long FileSizeBytes { get; set; }
+    public int CourseId { get; set; }
+    public int LessonId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
